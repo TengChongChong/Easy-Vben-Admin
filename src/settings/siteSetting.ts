5369b37846bdcac1bff97916129df561e5942f2e
TengChongChong <26172583@qq.com>
2022-05-12 17:23:07 +0800

    docs: 修改注释

--- src/settings/siteSetting.ts
+++ src/settings/siteSetting.ts
@@ -1,8 +1,8 @@
-// github repo url
+// github 地址
 export const GITHUB_URL = 'https://github.com/anncwb/vue-vben-admin';
 
-// vue-vben-admin-next-doc
+// 文档
 export const DOC_URL = 'https://vvbin.cn/doc-next/';
 
-// site url
+// 站点
 export const SITE_URL = 'https://vvbin.cn/next/';
