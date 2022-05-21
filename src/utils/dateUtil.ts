a8dc9442b98feefbce7b33a756cedebe82bacec7
TengChongChong <26172583@qq.com>
2022-05-21 16:29:17 +0800

    chore: 添加支持类型

--- src/utils/dateUtil.ts
+++ src/utils/dateUtil.ts
@@ -7,14 +7,14 @@ const DATE_TIME_FORMAT = 'YYYY-MM-DD HH:mm:ss';
 const DATE_FORMAT = 'YYYY-MM-DD';
 
 export function formatToDateTime(
-  date: dayjs.Dayjs | undefined = undefined,
+  date: dayjs.Dayjs | Date | undefined = undefined,
   format = DATE_TIME_FORMAT,
 ): string {
   return dayjs(date).format(format);
 }
 
 export function formatToDate(
-  date: dayjs.Dayjs | undefined = undefined,
+  date: dayjs.Dayjs | string | undefined = undefined,
   format = DATE_FORMAT,
 ): string {
   return dayjs(date).format(format);
