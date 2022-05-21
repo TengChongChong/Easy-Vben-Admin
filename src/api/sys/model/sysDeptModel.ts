94a016a4256b08ee53ef7f4b3e3180181989c541
TengChongChong <26172583@qq.com>
2022-05-21 16:15:29 +0800

    chore: 调整会话相关代码

---
+++ src/api/sys/model/sysDeptModel.ts
@@ -0,0 +1,23 @@
+import { BasicModel } from '/@/api/model/baseModel';
+
+/**
+ * 部门
+ */
+export interface SysDept extends BasicModel {
+  // 全称
+  name: string;
+  // 简称
+  simpleName: string;
+  // 部门类型编码
+  typeCode: string;
+  //部门代码
+  code: string;
+  // 备注
+  remarks: string;
+  // 排序值
+  orderNo: number;
+  // 部门类型名称
+  typeName: string;
+  // 上级部门名称
+  parentName: string;
+}
