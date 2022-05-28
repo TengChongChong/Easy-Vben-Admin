3c9b61568387138faa02a19df76106413b1f85a2
TengChongChong <26172583@qq.com>
2022-05-28 10:43:57 +0800

    chore: 添加展开选中节点

--- src/components/Form/src/components/ApiTreeSelect.vue
+++ src/components/Form/src/components/ApiTreeSelect.vue
@@ -1,5 +1,13 @@
 <template>
-  <a-tree-select v-bind="getAttrs" @change="handleChange">
+  <a-tree-select
+    v-bind="getAttrs"
+    show-search
+    :tree-node-filter-prop="treeNodeFilterProp"
+    @tree-expand="treeExpand"
+    @change="handleChange"
+    v-model:treeExpandedKeys="treeExpandedKeys"
+    v-model:value="state"
+  >
     <template #[item]="data" v-for="item in Object.keys($slots)">
       <slot :name="item" v-bind="data || {}"></slot>
     </template>
@@ -12,30 +20,41 @@
 <script lang="ts">
   import { computed, defineComponent, watch, ref, onMounted, unref } from 'vue';
   import { TreeSelect } from 'ant-design-vue';
-  import { isArray, isFunction } from '/@/utils/is';
+  import { isArray, isFunction, isNullOrUnDef, isNumber, isString } from '/@/utils/is';
   import { get } from 'lodash-es';
   import { propTypes } from '/@/utils/propTypes';
   import { LoadingOutlined } from '@ant-design/icons-vue';
+  import { useRuleFormItem } from '/@/hooks/component/useFormItem';
+  import { findPath } from '/@/utils/helper/treeHelper';
   export default defineComponent({
     name: 'ApiTreeSelect',
     components: { ATreeSelect: TreeSelect, LoadingOutlined },
     props: {
+      value: [Array, String, Number],
       api: { type: Function as PropType<(arg?: Recordable) => Promise<Recordable>> },
       params: { type: Object },
       immediate: { type: Boolean, default: true },
       resultField: propTypes.string.def(''),
+      // 搜索字段
+      treeNodeFilterProp: propTypes.string.def('title'),
+      afterFetch: { type: Function as PropType<Fn> },
     },
     emits: ['options-change', 'change'],
     setup(props, { attrs, emit }) {
       const treeData = ref<Recordable[]>([]);
       const isFirstLoaded = ref<Boolean>(false);
       const loading = ref(false);
+      const treeExpandedKeys = ref([]);
+      const emitData = ref<any[]>([]);
       const getAttrs = computed(() => {
         return {
           ...(props.api ? { treeData: unref(treeData) } : {}),
           ...attrs,
         };
       });
+
+      // Embedded in the form, just use the hook binding to perform form verification
+      const [state] = useRuleFormItem(props, 'value', 'change', emitData);
 
       function handleChange(...args) {
         emit('change', ...args);
@@ -56,12 +75,19 @@
         },
       );
 
+      watch(
+        () => props.value,
+        () => {
+          setExpandedKeys();
+        },
+      );
+
       onMounted(() => {
         props.immediate && fetch();
       });
 
       async function fetch() {
-        const { api } = props;
+        const { api, afterFetch } = props;
         if (!api || !isFunction(api)) return;
         loading.value = true;
         treeData.value = [];
@@ -71,6 +97,9 @@
         } catch (e) {
           console.error(e);
         }
+        if (afterFetch && isFunction(afterFetch)) {
+          result = afterFetch(result);
+        }
         loading.value = false;
         if (!result) return;
         if (!isArray(result)) {
@@ -79,8 +108,38 @@
         treeData.value = (result as Recordable[]) || [];
         isFirstLoaded.value = true;
         emit('options-change', treeData.value);
+        setExpandedKeys();
       }
-      return { getAttrs, loading, handleChange };
+
+      /**
+       * 展开选中节点
+       */
+      function setExpandedKeys() {
+        treeExpandedKeys.value = [];
+
+        if (treeData.value.length === 0 || isNullOrUnDef(props.value)) {
+          return;
+        }
+
+        let pathArray: any[] = [];
+        // value 有值
+        if (isString(props.value) || isNumber(props.value)) {
+          pathArray = findPath(treeData.value, (n) => n.id === props.value);
+        }
+        if (pathArray.length > 1) {
+          // 去掉最后一级（当前节点不需要展开）
+          pathArray = pathArray.slice(0, pathArray.length - 1);
+          pathArray.map((item) => {
+            // @ts-ignore
+            treeExpandedKeys.value.push(item.key);
+          });
+        }
+      }
+
+      function treeExpand(keys) {
+        treeExpandedKeys.value = keys;
+      }
+      return { getAttrs, loading, state, treeExpandedKeys, treeExpand, handleChange };
     },
   });
 </script>
