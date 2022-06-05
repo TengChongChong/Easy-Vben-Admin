f479bfbad148d0981f6e07641ca55f3799539367
TengChongChong <26172583@qq.com>
2022-06-05 20:03:38 +0800

    chore: 调整图标引入方式

--- src/components/Form/src/components/FormAction.vue
+++ src/components/Form/src/components/FormAction.vue
@@ -51,7 +51,7 @@
   import { useFormContext } from '../hooks/useFormContext';
   import { useI18n } from '/@/hooks/web/useI18n';
   import { propTypes } from '/@/utils/propTypes';
-  import Icon from '/@/components/Icon/src/Icon.vue';
+  import { Icon } from '/@/components/Icon';
 
   type ButtonOptions = Partial<ButtonProps> & { text: string };
 
