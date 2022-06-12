8559ac5287d0b09a774ee4e42a14dda1ea776e38
TengChongChong <26172583@qq.com>
2022-06-12 15:38:25 +0800

    chore: 新增默认注册组件

--- src/components/registerGlobComp.ts
+++ src/components/registerGlobComp.ts
@@ -4,19 +4,25 @@ import {
   Alert,
   Avatar,
   Button,
+  Descriptions,
   Layout,
+  List,
   Card,
   Cascader,
   Divider,
   Dropdown,
+  Menu,
   Tag,
   Tree,
   Tooltip,
+  Typography,
   Popconfirm,
   Input,
   Radio,
   Row,
+  Result,
   Select,
+  Spin,
   Col,
 } from 'ant-design-vue';
 
@@ -25,18 +31,24 @@ export function registerGlobComp(app: App) {
     .use(Alert)
     .use(Avatar)
     .use(Button)
+    .use(Descriptions)
     .use(Layout)
+    .use(List)
     .use(Card)
     .use(Cascader)
     .use(Divider)
     .use(Dropdown)
+    .use(Menu)
     .use(Tag)
     .use(Tree)
     .use(Tooltip)
+    .use(Typography)
     .use(Popconfirm)
     .use(Input)
     .use(Radio)
     .use(Row)
+    .use(Result)
     .use(Select)
+    .use(Spin)
     .use(Col);
 }
