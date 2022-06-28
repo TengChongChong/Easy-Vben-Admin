b58932439857e2a7d1eda338fe74e459d2f452f8
TengChongChong <26172583@qq.com>
2022-06-28 22:03:40 +0800

    ci: 调整默认引入组件

--- src/components/registerGlobComp.ts
+++ src/components/registerGlobComp.ts
@@ -1,9 +1,11 @@
 import type { App } from 'vue';
-// import { Button } from './Button';
+
 import {
   Alert,
   Avatar,
   Button,
+  Badge,
+  Checkbox,
   Descriptions,
   Layout,
   List,
@@ -12,12 +14,17 @@ import {
   Divider,
   Dropdown,
   Menu,
+  Table,
+  Tabs,
   Tag,
   Tree,
   Tooltip,
   Typography,
   Popconfirm,
+  Popover,
   Input,
+  InputNumber,
+  Upload,
   Radio,
   Row,
   Result,
@@ -31,20 +38,27 @@ export function registerGlobComp(app: App) {
     .use(Alert)
     .use(Avatar)
     .use(Button)
+    .use(Badge)
     .use(Descriptions)
     .use(Layout)
     .use(List)
     .use(Card)
     .use(Cascader)
+    .use(Checkbox)
     .use(Divider)
     .use(Dropdown)
     .use(Menu)
+    .use(Table)
+    .use(Tabs)
     .use(Tag)
     .use(Tree)
     .use(Tooltip)
     .use(Typography)
     .use(Popconfirm)
+    .use(Popover)
     .use(Input)
+    .use(InputNumber)
+    .use(Upload)
     .use(Radio)
     .use(Row)
     .use(Result)
