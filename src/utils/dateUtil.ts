4590e09a8d55cc898a9b73fa5c7089d2b813df0e
TengChongChong <26172583@qq.com>
2022-06-28 22:12:26 +0800

    ci: 添加relativeTime/locale_zh/formatToNow

--- src/utils/dateUtil.ts
+++ src/utils/dateUtil.ts
@@ -2,6 +2,11 @@
  * Independent time operation tool to facilitate subsequent switch to dayjs
  */
 import dayjs from 'dayjs';
+import locale_zh from 'dayjs/locale/zh-cn';
+dayjs.locale(locale_zh);
+// RelativeTime 插件
+import relativeTime from 'dayjs/plugin/relativeTime';
+dayjs.extend(relativeTime);
 
 const DATE_TIME_FORMAT = 'YYYY-MM-DD HH:mm:ss';
 const DATE_FORMAT = 'YYYY-MM-DD';
@@ -18,6 +23,17 @@ export function formatToDate(
   format = DATE_FORMAT,
 ): string {
   return dayjs(date).format(format);
+}
+
+export function formatToNow(date: dayjs.Dayjs | Date | undefined = undefined): string {
+  if (isLongAgo(date)) {
+    return dayjs(date).format('YYYY-MM-DD HH:mm');
+  }
+  return dayjs(date).fromNow();
+}
+
+function isLongAgo(date: dayjs.Dayjs | Date | undefined = undefined) {
+  return dayjs(new Date()).diff(date) > 1000 * 60 * 60 * 24 * 30;
 }
 
 export const dateUtil = dayjs;
