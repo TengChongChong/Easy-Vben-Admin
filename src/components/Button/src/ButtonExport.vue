5b2050f828a6efc4de1651b7ceaa80b534aa7ecd
TengChongChong <26172583@qq.com>
2022-06-28 21:44:31 +0800

    feat: 添加常用按钮

---
+++ src/components/Button/src/ButtonExport.vue
@@ -0,0 +1,36 @@
+<template>
+  <Authority :value="auth">
+    <a-button :loading="loading" @click="handleClick">
+      <template #icon>
+        <Icon icon="ant-design:cloud-download-outlined" />
+      </template>
+      {{ text }}
+    </a-button>
+  </Authority>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue';
+  import { Icon } from '/@/components/Icon';
+  import { propTypes } from '/@/utils/propTypes';
+  import { Authority } from '/@/components/Authority';
+  export default defineComponent({
+    name: 'AButtonExport',
+    components: { Authority, Icon },
+    props: {
+      auth: propTypes.string,
+      text: propTypes.string.def('导出'),
+      loading: propTypes.bool.def(false),
+    },
+    emits: ['click'],
+    setup(_, { emit }) {
+      const handleClick = () => {
+        emit('click');
+      };
+
+      return {
+        handleClick,
+      };
+    },
+  });
+</script>
---
+++ src/components/Button/src/ButtonInfo.vue
@@ -0,0 +1,46 @@
+<template>
+  <Authority :value="auth">
+    <a-tooltip>
+      <template #title>{{ text }}</template>
+      <a-button type="link" :loading="loading" size="small" @click="handleClick">
+        <template #icon>
+          <Icon icon="ant-design:search-outlined" />
+        </template>
+      </a-button>
+    </a-tooltip>
+  </Authority>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue';
+  import { Icon } from '/@/components/Icon';
+  import { propTypes } from '/@/utils/propTypes';
+  import { useGo } from '/@/hooks/web/usePage';
+  import { Authority } from '/@/components/Authority';
+  export default defineComponent({
+    name: 'AButtonInfo',
+    components: { Authority, Icon },
+    props: {
+      auth: propTypes.string,
+      id: propTypes.string,
+      path: propTypes.string,
+      loading: propTypes.bool.def(false),
+      text: propTypes.string.def('详情'),
+    },
+    emits: ['click'],
+    setup(props, { emit }) {
+      const go = useGo();
+
+      const handleClick = () => {
+        emit('click', props.id);
+        if (props.path) {
+          go(props.path);
+        }
+      };
+
+      return {
+        handleClick,
+      };
+    },
+  });
+</script>
