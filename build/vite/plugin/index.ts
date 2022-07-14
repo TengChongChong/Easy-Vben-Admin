a6403eb121b96b9cb31fab29587d040e2158998e
TengChongChong <26172583@qq.com>
2022-07-14 21:04:13 +0800

    chore: 停用configHtmlPlugin

--- build/vite/plugin/index.ts
+++ build/vite/plugin/index.ts
@@ -6,9 +6,8 @@ import purgeIcons from 'vite-plugin-purge-icons';
 import windiCSS from 'vite-plugin-windicss';
 import VitePluginCertificate from 'vite-plugin-mkcert';
 import vueSetupExtend from 'vite-plugin-vue-setup-extend';
-import { configHtmlPlugin } from './html';
+// import { configHtmlPlugin } from './html';
 import { configPwaConfig } from './pwa';
-import { configMockPlugin } from './mock';
 import { configCompressPlugin } from './compress';
 import { configStyleImportPlugin } from './styleImport';
 import { configVisualizerConfig } from './visualizer';
@@ -19,7 +18,6 @@ import { configSvgIconsPlugin } from './svgSprite';
 export function createVitePlugins(viteEnv: ViteEnv, isBuild: boolean) {
   const {
     VITE_USE_IMAGEMIN,
-    VITE_USE_MOCK,
     VITE_LEGACY,
     VITE_BUILD_COMPRESS,
     VITE_BUILD_COMPRESS_DELETE_ORIGIN_FILE,
@@ -43,14 +41,11 @@ export function createVitePlugins(viteEnv: ViteEnv, isBuild: boolean) {
   // @vitejs/plugin-legacy
   VITE_LEGACY && isBuild && vitePlugins.push(legacy());
 
-  // vite-plugin-html
-  vitePlugins.push(configHtmlPlugin(viteEnv, isBuild));
+  // vite-plugin-html，开启会导致无法直接访问public目录下文件
+  // vitePlugins.push(configHtmlPlugin(viteEnv, isBuild));
 
   // vite-plugin-svg-icons
   vitePlugins.push(configSvgIconsPlugin(isBuild));
-
-  // vite-plugin-mock
-  VITE_USE_MOCK && vitePlugins.push(configMockPlugin(isBuild));
 
   // vite-plugin-purge-icons
   vitePlugins.push(purgeIcons());
