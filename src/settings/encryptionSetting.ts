3d1142c50df599868aee799777be7a950fd74633
TengChongChong <26172583@qq.com>
2022-07-26 20:06:17 +0800

    ci: 调整缓存过期时间

--- src/settings/encryptionSetting.ts
+++ src/settings/encryptionSetting.ts
@@ -1,7 +1,7 @@
 import { isDevMode } from '/@/utils/env';
 
-// 缓存默认过期时间 7 天
-export const DEFAULT_CACHE_TIME = 60 * 60 * 24 * 7;
+// 缓存默认过期时间 14 天
+export const DEFAULT_CACHE_TIME = 60 * 60 * 24 * 14;
 
 // 开启缓存加密后，加密密钥。采用aes加密
 export const cacheCipher = {
